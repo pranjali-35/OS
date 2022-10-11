e38cf0281119ca96aea9cfa2f32fcf00d1746820
pranjali-35 <91174464+pranjali-35@users.noreply.github.com>
2022-10-12 00:40:40 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -42,12 +42,16 @@ int main(){
 	a[0].wt = a[0].tat - a[0].bt;
 	a[0].rt = a[0].st - a[0].at;
 
+	int idle_time = 0;
 	for(int i = 1 ; i < n ; i++){
 		a[i].st = (a[i-1].ct > a[i].at) ? a[i-1].ct : a[i].at;
 		a[i].ct = a[i].st + a[i].bt;
 		a[i].tat = a[i].ct - a[i].at;
 		a[i].wt = a[i].tat - a[i].bt;
 		a[i].rt = a[i].st - a[i].at;
+
+		if(a[i].st > a[i-1].ct)
+			idle_time += (a[i-1].ct - a[i].st);
 	}
 
 	float avg_tat = 0, avg_wt = 0, avg_rt = 0;
@@ -61,6 +65,11 @@ int main(){
 	avg_wt /= n;
 	avg_rt /= n;
 
+	float cpu_utl = ((a[n-1].ct - idle_time)/a[n-1].ct)*100;
+	float throughput = n/(float)a[n-1].ct;
+
+	printf("CPU Utilization = %f\n",cpu_utl);
+	printf("Through put = %f\n",throughput);
 	printf("Average TAT = %f\n",avg_tat);
 	printf("Average WT = %f\n",avg_wt);
 	printf("Average RT = %f\n",avg_rt);
